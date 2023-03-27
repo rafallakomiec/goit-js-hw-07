163ed0182dd4510d088cf720ad51ca5214f4e8af
rafallakomiec <114759971+rafallakomiec@users.noreply.github.com>
2023-03-27 21:14:22 +0200

    task 1 complete

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+'use strict';
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector('.gallery');
+const galleryHTMLArray = [];
+
+for (const item of galleryItems) {
+  const html = `<li><div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div></li>`;
+  galleryHTMLArray.push(html);
+}
+
+gallery.insertAdjacentHTML('beforeend', galleryHTMLArray.join(''));
+
+gallery.addEventListener('click', event => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+        <img src="${event.target.dataset.source}" width="800" height="600">
+    `);
+
+  function closeBox(event) {
+    if (event.code === 'Escape') {
+      instance.close(() => {
+        document.removeEventListener('keyup', closeBox);
+      });
+    }
+  }
+
+  instance.show();
+  document.addEventListener('keyup', closeBox);
+  instance
+    .element()
+    .addEventListener('click', () => document.removeEventListener('keyup', closeBox));
+});
